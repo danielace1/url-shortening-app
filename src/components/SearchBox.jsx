c8bd48e5474fe8b0ea46bd5aa706e1ff4d85208d
danielace1 <danielsudharsan1805@gmail.com>
2024-07-06 02:12:50 +0530

    feat: zod-validation

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,20 +1,50 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const Schema = z.object({
+  search: z.string().url({ message: "Please add a valid link" }),
+});
+
 const SearchBox = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ resolver: zodResolver(Schema) });
+
+  const submitform = (e) => {
+    console.log(e);
+    reset();
+  };
   return (
     <div className="mx-72 bg-[url('/bg-shorten-desktop.svg')] bg-no-repeat bg-cover rounded-lg">
-      <div className="bg-DarkViolet bg-opacity-80 px-12 py-10 rounded-lg">
+      <form
+        onSubmit={handleSubmit(submitform)}
+        className="bg-DarkViolet bg-opacity-80 px-12 py-10 h-32 rounded-lg"
+      >
         <div className="flex items-center space-x-4">
           <input
             type="text"
             name="search"
             id="search"
             placeholder="Shorten a link here..."
-            className="outline-none w-5/6 px-5 py-2.5 font-semibold rounded-md focus:border focus:border-GreyishViolet"
+            className={`px-5 py-2.5  outline-none rounded-md w-5/6 focus:border-2   ${
+              errors?.search ? "border  border-red-400" : "focus:border-Grey"
+            }`}
+            {...register("search")}
           />
-          <button className="text-white w-1/6 px-6 py-2.5 bg-Cyan hover:bg-cyan-200 font-bold text-lg rounded-md">
+          <button className="w-1/6 text-white px-6 py-2.5 bg-Cyan hover:bg-cyan-200 font-bold text-lg rounded-md">
             Shorten It!
           </button>
         </div>
-      </div>
+        {errors?.search && (
+          <small className="text-red-500 text-sm">
+            {errors.search?.message}
+          </small>
+        )}
+      </form>
     </div>
   );
 };
